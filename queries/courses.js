2a88b05c6d8946cc3d4bc01325429f5723148513
Noushadozi <noushadozi333@gmail.com>
2024-06-08 20:23:07 +0600

    landing page, course list

--- queries/courses.js
+++ queries/courses.js
@@ -1,11 +1,21 @@
+import { replaceMongoIdInArray } from "@/lib/convertData";
 import { Category } from "@/model/category-model";
 import { Course } from "@/model/course-model";
 import { Module } from "@/model/module.model";
 import { Testimonial } from "@/model/testimonial-model";
 import { User } from "@/model/user-model";
 
-export async function getCourses() {
+export async function getCourseList() {
   const courses = await Course.find({})
+    .select([
+      "title",
+      "subtitle",
+      "thumbnail",
+      "modules",
+      "price",
+      "category",
+      "instructor",
+    ])
     .populate({
       path: "category",
       model: Category,
@@ -21,6 +31,8 @@ export async function getCourses() {
     .populate({
       path: "modules",
       model: Module,
-    });
-  return courses;
+    })
+    .lean();
+
+  return replaceMongoIdInArray(courses);
 }
